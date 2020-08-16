7fa7f9840c2022dc9ba7a1639f9c37791ec06765
Your Name <you@example.com>
2020-08-17 00:51:39 +0800

    update

---
+++ deploy/files/conf/airflow/create_user.py
@@ -0,0 +1,13 @@
+# create airflow login user
+
+import airflow
+from airflow import models, settings
+from airflow.contrib.auth.backends.password_auth import PasswordUser
+user = PasswordUser(models.User())
+user.username = 'airflow'
+user.email = 'new@example.com'
+user.password = 'airflow'
+session = settings.Session()
+session.add(user)
+session.commit()
+session.close()
